e451334381c436b1dcba905828fced598cb62ea9
Sam Goldsmith <samggold@gmail.com>
2016-09-19 08:39:55 -0600

    heroku requiring dotenv, made change to that

--- src/server/config/main-config.js
+++ src/server/config/main-config.js
@@ -18,7 +18,9 @@
   ];
 
   // *** load environment variables *** //
-  require('dotenv').config();
+  if (process.env.NODE_ENV === 'test' || 'development') {
+    require('dotenv').config();
+  }
 
   appConfig.init = function(app, express) {
 
